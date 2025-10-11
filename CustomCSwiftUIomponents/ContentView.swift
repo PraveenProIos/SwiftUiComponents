79900aa83d656bf7d4fd55b75383d0ee43072a35
PraveenProIos <praveenios.p@gmail.com>
2025-10-11 12:47:21 +0530

    project adding

---
+++ CustomCSwiftUIomponents/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CustomCSwiftUIomponents
+//
+//  Created by Praveen on 11/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+          Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomCSwiftUIomponents/CustomCSwiftUIomponentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCSwiftUIomponentsApp.swift
+//  CustomCSwiftUIomponents
+//
+//  Created by Praveen on 11/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomCSwiftUIomponentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
