aae91b8b6fdcd34453a131331340af358e42b4c2
PraveenProIos <praveenios.p@gmail.com>
2025-10-11 22:41:32 +0530

    File3 added.

---
+++ CustomCSwiftUIomponents/NewFile3.swift
@@ -0,0 +1,20 @@
+//
+//  NewFile3.swift
+//  CustomCSwiftUIomponents
+//
+//  Created by Praveen on 11/10/25.
+//
+
+import SwiftUI
+
+struct NewFile3: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NewFile3_Previews: PreviewProvider {
+    static var previews: some View {
+        NewFile3()
+    }
+}
